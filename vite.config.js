df0fb0c163f959a7629d7abeb07f06b73401326b
sakshi-exe <sakshi14506@gmail.com>
2026-08-30 09:24:36 +0530

    fix render deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173
-  }
+
+  preview: {
+    host: '0.0.0.0',
+    port: 10000,
+    allowedHosts: true,
+  },
 })
